fa278e86cc6200c9833922a6b003ef118a541e68
Bin <kyhuuha@gmail.com>
2016-10-28 14:25:41 +0700

    init Project

---
+++ phuot/src/main/java/cntt/duytan/controller/HomeController.java
@@ -0,0 +1,40 @@
+package cntt.duytan.controller;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+@RequestMapping(value="/")
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home", method = RequestMethod.GET)
+	public ModelAndView home(Locale locale, ModelAndView model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addObject("serverTime", formattedDate );
+		model.setViewName("home");
+		return model;
+	}
+	
+}
---
+++ phuot/src/main/java/cntt/duytan/security/LoginFailureHandler.java
@@ -0,0 +1,26 @@
+package cntt.duytan.security;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+
+public class LoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+	@Override
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+			AuthenticationException exception) throws IOException, ServletException {
+		String name = request.getParameter("username");
+		
+		
+		
+		exception.printStackTrace();
+		super.onAuthenticationFailure(request, response, exception);
+	}
+	
+}
